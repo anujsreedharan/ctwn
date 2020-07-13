a660ed72f88e24647e230f442562e8b33bed4eaf
anujsreedharan <54094596+anujsreedharan@users.noreply.github.com>
2020-07-13 10:27:40 -0400

    full working discord countdown bot

--- main.js
+++ main.js
@@ -7,8 +7,25 @@ args: ['--no-sandbox']
 const page = await browser.newPage();
 await page.goto('https://www.livechart.me/anime/9733');
 await page.waitForXPath("/html/body/div[3]/div/div[2]/div[2]/div[3]/div[1]");
+await page.waitForXPath("/html/body/div[3]/div/div[2]/div[2]/div[4]/div[1]");
+await page.waitForXPath("/html/body/div[3]/div/div[2]/div[2]/div[5]/div[1]");
+await page.waitForXPath("/html/body/div[3]/div/div[2]/div[2]/div[1]");
 let elHandle = await page.$x("/html/body/div[3]/div/div[2]/div[2]/div[3]/div[1]");
+let deHandle = await page.$x("/html/body/div[3]/div/div[2]/div[2]/div[1]")
+let ebHandle = await page.$x("/html/body/div[3]/div/div[2]/div[2]/div[4]/div[1]")
+
+let esHandle = await page.$x("/html/body/div[3]/div/div[2]/div[2]/div[5]/div[1]")
+
+let scdsf = await page.evaluate(de => de.textContent, deHandle[0]);
 let asasa = await page.evaluate(el => el.textContent, elHandle[0]);
+
+let bsbsb = await page.evaluate(ed => ed.textContent,ebHandle[0]);
+
+let cscsc = await page.evaluate(es => es.textContent,esHandle[0]);
+
 console.log(asasa,'days');
+console.log(bsbsb, 'hours');
+console.log(cscsc, 'minutes');
+console.log(scdsf)
 await browser.close()
 })();
