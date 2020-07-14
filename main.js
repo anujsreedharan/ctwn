fbea31b4f457d8978f51f43d36bc42615b8a1d44
anujsreedharan <54094596+anujsreedharan@users.noreply.github.com>
2020-07-14 12:46:18 -0400

    Update main.js
    added fire force season 2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const puppeteer = require("puppeteer");
 
 const prefix = ",";
 discordClient.login(
-  process.env.TOKEN
+  "NjkwOTU3NzIzMzA2NTU3NDkw.XnZEmw.f9aQr2_-Pp2Vk3qV-kxxfnecYso"
 );
 discordClient.on("ready", () => {
   console.log(`Logged in as ${discordClient.user.tag}!`);
@@ -339,11 +339,73 @@ discordClient.on("message", message => {
           embed: {
             color: 0xdb1e4a,
             title: "Airing Series commands",
-            description:("`,sao` - Sword Art Online \n `,op` - One Piece \n `,aot` - Attack on Titan: Chronicle \n `,goh` - The God of Highschool \n `,rez` - Re:Zero season 2 part 1 ")
+            description:("`,sao` - Sword Art Online \n`,op` -  One Piece \n`,aot` - Attack on Titan: Chronicle \n`,goh` - The God of Highschool\n `,rez` - Re:Zero Season 2 Part 1 \n`,enn` - Enn Enn no Shouboutai: Ni no Shou")
         }
           
       });
-       }
+       }else if (command === "enn") {
+        message.react('✅').then(async f => {
+        const browser = await puppeteer.launch({
+          headless: true,
+          args: ["--no-sandbox"]
+        });
+        const page = await browser.newPage();
+        await page.goto("https://www.livechart.me/anime/9746");
+        //days
+        await page.waitForXPath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[3]/div[1]"
+        );
+        //hours
+        await page.waitForXPath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[4]/div[1]"
+        );
+        //minutes
+        await page.waitForXPath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[5]/div[1]"
+        );
+        //episode
+        await page.waitForXPath("/html/body/div[3]/div/div[2]/div[2]/div[1]");
+        let elHandle = await page.$x(
+          "/html/body/div[3]/div/div[2]/div[2]/div[3]/div[1]"
+        );
+        let ebHandle = await page.$x(
+          "/html/body/div[3]/div/div[2]/div[2]/div[4]/div[1]"
+        );
+        let esHandle = await page.$x(
+          "/html/body/div[3]/div/div[2]/div[2]/div[5]/div[1]"
+        );
+        let deHandle = await page.$x(
+          "/html/body/div[3]/div/div[2]/div[2]/div[1]"
+        );
+        let asasa = await page.evaluate(el => el.textContent, elHandle[0]);
+        let bsbsb = await page.evaluate(ed => ed.textContent, ebHandle[0]);
+        let cscsc = await page.evaluate(es => es.textContent, esHandle[0]);
+        let scdsf = await page.evaluate(de => de.textContent, deHandle[0]);
+        console.log(asasa, "days");
+        console.log(bsbsb, "hours");
+        console.log(cscsc, "minutes");
+        console.log(scdsf, "episode");
+        let rers = " days ";
+        let timem = asasa.concat(rers);
+        let sers = " hours ";
+        let dskd = bsbsb.concat(sers);
+        let sww = " minutes ";
+        let dsad = cscsc.concat(sww);
+        let ctw = "";
+        let last = "till next episode ";
+        let count = ctw.concat(timem, dskd, dsad, last);
+        let ti = "  of Enn Enn no Shouboutai: Ni no Shou";
+        let title = scdsf.concat(ti);
+        message.channel.send({
+          embed: {
+            color: 0xdb1e4a,
+            title: title,
+            description: ("Days:", count)
+          }
+        });
+        await browser.close();
+      });
 
   }
-  });
+  };
+});
